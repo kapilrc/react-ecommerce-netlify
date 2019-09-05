714f90d4d6694c161206c5d2762116f1020eb21c
kapilrc <kapilrc@gmail.com>
2019-09-05 18:32:52 +0530

    Initial setup. Added components and routes

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import logo from './../logo.svg';
+
+
+export default class Navbar extends Component {
+  render() {
+    return (
+      <div>        
+        <h3>Hello from Navbar</h3>
+      </div>
+    )
+  }
+}
