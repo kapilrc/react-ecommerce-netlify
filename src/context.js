182f4f0733c8be6e4147061f44aa0e059af4d3de
kapilrc <kapilrc@gmail.com>
2019-09-08 00:33:43 +0530

    Add product in the cart and also show the product in cart in product details page

--- src/context.js
+++ src/context.js
@@ -8,7 +8,8 @@ const ProductContext = React.createContext();
 class ProductProvider extends Component {
   state={
     products: [],
-    detailProduct: detailProduct
+    detailProduct: detailProduct,
+    cart: []
   };
   componentDidMount() {
     this.setProducts();
@@ -23,17 +24,35 @@ class ProductProvider extends Component {
       return {products: tempProducts};
     });
   }
-  handleDetails = () => {
-    console.log('Hello from handle details');
+  getItem = id => {
+    const product = this.state.products.find(item => item.id === id);
+    return product;
+  }
+  handleDetail = id => {
+    const product = this.getItem(id);
+    this.setState(() => {
+      return { detailProduct:product };
+    });
   };
-  addToCart = (id) => {
-    console.log(`Hello from add to cart.id ${id}`);
+  addToCart = id => {
+    const tempProducts = [...this.state.products];
+    const index = tempProducts.indexOf(this.getItem(id));
+    const product = tempProducts[index];
+    product.inCart = true;
+    product.count = 1;
+    const price = product.price;
+    product.total = price;
+    this.setState(
+      () => {
+        return { products: tempProducts, cart: [...this.state.cart, product] };
+      }, ()=> { console.log( this.state ) }
+    )
   };
   render() {
     return (
       <ProductContext.Provider value={{
         ...this.state, 
-        handleDetails: this.handleDetails,
+        handleDetail: this.handleDetail,
         addToCart: this.addToCart
       }}>
       {this.props.children}
