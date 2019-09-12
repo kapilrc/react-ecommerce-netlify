a366bbd41d4c942cd182995748fd9029f8956a0c
kapilrc <kapilrc@gmail.com>
2019-09-12 16:41:50 +0530

    removeItem and clearCart functionality done

--- src/context.js
+++ src/context.js
@@ -73,15 +73,38 @@ class ProductProvider extends Component {
     console.log('this is decrement Method');
   };
   removeItem = id => {
-    console.log('item removed');
+    let tempProducts = [...this.state.products];
+    let tempCart = [...this.state.cart];
+
+    tempCart = tempCart.filter(item => item.id !== id);
+
+    const index = tempProducts.indexOf(this.getItem(id));
+    let removedProduct = tempProducts[index];
+    removedProduct.inCart = false;
+    removedProduct.count = 0;
+    removedProduct.total = 0;
+
+    this.setState(() => {
+      return {
+        cart: [...tempCart],
+        products: [...tempProducts]
+      };
+    },() => {
+      this.addTotals();
+    })
   };
   clearCart = () => {
-    console.log('Cart was cleared');
+    this.setState(() => {
+      return {cart: []}
+    }, () => {
+      this.setProducts();
+      this.addTotals();
+    });
   };
   addTotals = () => {
     let subTotal = 0;
     this.state.cart.map(item => (subTotal += item.total));
-    const tempTax = subTotal * 0.18;
+    const tempTax = subTotal * 0.1;
     const tax = parseFloat(tempTax.toFixed(2));
     const total = subTotal + tax;
     this.setState(() => {
